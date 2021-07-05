1b4f6b3d35f39ce30c967449cf04d61280f894c4
Ricardo Lara <rlara@utexas.edu>
2021-07-05 13:28:44 -0500

    Added validation with analytical solution

--- src/IMPES.jl
+++ src/IMPES.jl
@@ -1,17 +1,17 @@
 module IMPES
 
-# Write your package code here.
+using LinearAlgebra
+
 include("types.jl")
 include("flowFunctions.jl")
 include("T_matrix.jl")
 include("D_matrix.jl")
 include("Q_vector.jl")
-
+include("solvers.jl")
 
 export RelPerms, Reservoir, Fluids, Grid, BoundaryConditions,
-       Wells, Simulation
-
-export D_matrix, T_matrix, Q_matrix
+       Wells, Simulation, PostProcessing
+export D_matrix, T_matrix, Q_vector
 export IMPES!
 
 end
